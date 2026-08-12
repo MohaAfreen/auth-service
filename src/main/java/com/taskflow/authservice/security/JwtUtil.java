5a575071417f9af90e7361fd20638da5f6138d39
Afreen <mdafreen15042000@gmail.com>
2026-08-12 16:06:47 +0530

    removed gradle and used maven

--- src/main/java/com/taskflow/authservice/security/JwtUtil.java
+++ src/main/java/com/taskflow/authservice/security/JwtUtil.java
@@ -1,21 +1,50 @@
 package com.taskflow.authservice.security;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
     private final String SECRET="UniversalFavoriteAndSuperHandsomeSongjoongkiSecretKey12345";
-    public String generateToken(String username, String role, Long userId){
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(
+                SECRET.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    public String generateToken(
+            String username,
+            String role,
+            Long userId) {
+
         return Jwts.builder()
                 .setSubject(username)
-                .claim("role",role)
-                .claim("userId",userId)
+                .claim("role", role)
+                .claim("userId", userId)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60))
-                .signWith(SignatureAlgorithm.HS256,SECRET)
+                .setExpiration(
+                        new Date(
+                                System.currentTimeMillis()
+                                        + 1000 * 60 * 60
+                        )
+                )
+                .signWith(
+                        getSigningKey(),
+                        SignatureAlgorithm.HS256
+                )
                 .compact();
     }
+
+    @PostConstruct
+    public void debugSecret() {
+        System.out.println("AUTH JWT secret length = " + SECRET.length());
+        System.out.println("AUTH JWT secret hash = " + SECRET.hashCode());
+    }
+
 }
