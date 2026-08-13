11fceecb0cbbd9bf1c184080dae12a6bca14e708
Afreen <mdafreen15042000@gmail.com>
2026-08-13 14:59:21 +0530

    Added Global Exception Handling in Kafka Producer

--- src/main/java/com/taskflow/authservice/kafka/KafkaProducerService.java
+++ src/main/java/com/taskflow/authservice/kafka/KafkaProducerService.java
@@ -1,6 +1,8 @@
 package com.taskflow.authservice.kafka;
 
 import com.taskflow.authservice.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.annotation.JsonSerialize;
@@ -8,6 +10,7 @@ import tools.jackson.databind.annotation.JsonSerialize;
 @Service
 public class KafkaProducerService {
     private final KafkaTemplate<String,UserRegisteredEvent> kafkaTemplate;
+    private final Logger logger= LoggerFactory.getLogger(KafkaProducerService.class);
 
     public KafkaProducerService(KafkaTemplate<String, UserRegisteredEvent> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -20,7 +23,17 @@ public class KafkaProducerService {
                 .email(user.getEmail())
                         .build();
 
-        kafkaTemplate.send("user-registered",eventObj);
+        kafkaTemplate.send("user-registered",eventObj)
+                .whenComplete((result,ex)->{
+                    if(ex!=null){
+                        logger.error("Failed to send UserRegisteredEvent for user :{}",user.getUsername(),ex);
+                    }else{
+                       logger.info("UserRegistered event sent successfully {} {} {}",
+                               result.getRecordMetadata().topic(),
+                               result.getRecordMetadata().partition(),
+                               result.getRecordMetadata().offset());
+                    }
+                });
     }
 
 }
